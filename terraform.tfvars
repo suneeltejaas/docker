ef09805d2cc83d99957c63349d071e44056d24b6
Sunil Teja Sabbineni <sunilteja.sabbineni@everestdx.com>
2021-09-17 02:50:33 +0530

    docker instance

---
+++ terraform.tfvars
@@ -0,0 +1,13 @@
+name = "docker"
+region = "us-east-1"
+instancetype = "t2.medium"
+ami = "ami-09e67e426f25ce0d7"
+vpc = "vpc-79857b04"
+subnet = "subnet-0b42b09911e359286"
+default_tags = {
+        "Application":"Docker",
+        "Environment":"Container",
+  }
+keypair = "suneel-test"
+sshSecurityGroup = "sg-0171b9eb291b9d915"
+addlDiskSizeinGB = 20
---
+++ user-data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce=5:20.10.8~3-0~ubuntu-focal docker-ce-cli=5:20.10.8~3-0~ubuntu-focal containerd.io
